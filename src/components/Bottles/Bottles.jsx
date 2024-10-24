e41de8d2bfc22bc91f429455e31a752f12aae5f4
saideeWD <mdsaideehasan@gmail.com>
2024-10-24 20:24:04 +0600

    tailwend css

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -12,10 +12,10 @@ useEffect(()=>{
 },[])
 console.log(bottles)
   return (
-    <div>
+    <div className='mx-auto container'>
       
       <h2>Bottles Here {bottles.length}</h2>
-      <div className="conatiner">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 items-center  ">
       {
         bottles.map((item,index)=>(<Bottle key={index} items={item}/>))
       }</div>
